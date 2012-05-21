03d35cbeafee99d3e3b36966f828ebd330e4a14e
Gregg <gregg@wordnik.com>
2012-05-20 17:21:38 -0700

    brainstorming on some models

--- src/main/scala/com/taps/model/Model.scala
+++ src/main/scala/com/taps/model/Model.scala
@@ -8,11 +8,32 @@ import akka.util.Deadline._
 case class Beer(@Ignore id: Option[String],
                    description: Option[String],
                    name: Option[String],
-                   userUrl: Option[String])
+                   userUrl: Option[String],
+		   breweryId: String)
 
 case class User(@Ignore id: Option[String], 
 		@Ignore dateCreated: Option[Date], 
 		@Ignore lastUpdated: Option[Date], 
 		email: String, password: String)
 
+case class Brewery(@Ignore id: Option[String], 
+		   description: Option[String],
+		   name: String,
+		   location: Option[Location],
+		   website: Option[String])
+
+case class Place(@Ignore id: Option[String], 
+		   description: Option[String],
+		   name: String,
+		   location: Option[Location],
+		   website: Option[String],
+		   beers: Option[Seq[String]])
+
+case class Location(city: Option[String],
+		    state: Option[String],
+		    country: Option[String],
+		    streetAddress: Option[String],
+		    zip: Option[Int],
+		    lat: Option[Double],
+		    lon: Option[Double])
 
