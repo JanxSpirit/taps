f59cf4a37eeefe7b908dda5f1921d66c94c2f33d
Gregg <gregg@wordnik.com>
2012-05-31 21:42:11 -0700

    user beer service methods

--- src/main/scala/com/taps/service/UserBeerService.scala
+++ src/main/scala/com/taps/service/UserBeerService.scala
@@ -2,7 +2,7 @@ package com.taps.service
 import akka.dispatch.Future
 
 import com.taps.dao.{BeerDao, BreweryDao, UserDao}
-import com.taps.model.Beer
+import com.taps.model.{Beer, Brewery}
 
 trait UserBeerService {
   val beerDao: BeerDao
@@ -17,8 +17,19 @@ trait UserBeerService {
       case Some(u) => Future.sequence(u.favBeers.map(id => beerDao.getByKey(id)))
     }
 	
-  def getUnfavBeers: Seq[Beer]
-  def getFavBreweries: Seq[Beer]
-  def getUnfavBreweries: Seq[Beer]
+  def getUnfavBeers(userId: String): Future[List[Option[Beer]]] = 
+    userDao.getByKey(userId).flatMap {
+      case Some(u) => Future.sequence(u.unfavBeers.map(id => beerDao.getByKey(id)))
+    }
+
+  def getFavBreweries(userId: String): Future[List[Option[Brewery]]] = 
+    userDao.getByKey(userId).flatMap {
+      case Some(u) => Future.sequence(u.favBreweries.map(id => breweryDao.getByKey(id)))
+    }
+
+  def getUnfavBreweries(userId: String): Future[List[Option[Brewery]]] = 
+    userDao.getByKey(userId).flatMap {
+      case Some(u) => Future.sequence(u.unfavBreweries.map(id => breweryDao.getByKey(id)))
+    }
 
 }
